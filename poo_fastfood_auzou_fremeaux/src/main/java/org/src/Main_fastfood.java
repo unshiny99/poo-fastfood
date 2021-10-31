1a726944c675c09601fc9241ae0ef16d29a8f334
JoJo-fr <geoffrey.auzou@gmail.com>
2021-10-31 21:32:48 +0100

    Ajout vérification de l'existence du fichier HistoriqueCommande.json

--- poo_fastfood_auzou_fremeaux/src/main/java/org/src/Main_fastfood.java
+++ poo_fastfood_auzou_fremeaux/src/main/java/org/src/Main_fastfood.java
@@ -1,14 +1,36 @@
 package org.src;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import org.src.ObjetBorne.Borne;
 import org.src.ObjetBorne.Data.Data;
 import org.src.ObjetBorne.Thread.BorneCommandes;
 import org.src.ObjetBorne.Thread.Employer;
 
 public class Main_fastfood{
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("---------- FIS'eat ----------");
 
         Data.GenerateData();
+
+        // Vérification présence du fichier JsonEdit
+        // System.out.println(System.getProperty("user.dir"));
+        try{
+            File json = new File("./src/main/java/org/src/ObjetBorne/Data/HistoriqueCommandes.json");
+            if(!json.isFile()){
+                System.out.println("Le fichier HistoriqueCommandes.json n'éxiste pas");
+                FileWriter writer = new FileWriter(json);
+                json.createNewFile();
+                writer.write("{\n\n}");
+                writer.flush();
+                writer.close();
+                System.out.println("Génération terminé");
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        
         // Data.ecrireJSON("historique.json");
 
         BorneCommandes borneCommandes = new BorneCommandes();
